c2bb78eed10c9cc4df826a04697d38c0ffacc792
patrickz24 <patrick.zamblebi@yahoo.fr>
2021-11-29 09:28:44 +0100

    first step + start second step

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const fooBarqix = (value) => {
+    if (!value || typeof value !== "string") {
+        return "";
+    }
+
+    const numbers = [["3", "Foo"], ["5", "Bar"], ["7", "Qix"]];
+    let result= "";
+    numbers.forEach((num) => {
+        if (value % num[0] === 0) {
+           result += num[1];
+        }
+    });
+
+ 
+    value.split("").forEach(( number) => {
+         if ( number.includes('0')){
+             result+="*"
+         }
+        numbers.forEach((num) => {
+            if (num[0].includes( number)) {  
+               result += num[1];
+              
+            }
+
+           
+        });
+
+    });
+   
+    if (!str) {
+        console.log(value);
+        return value;
+    }
+    console.log(result);
+    return result;
+}
+
+
+fooBarqix("101");
